433e0d25be50089e96c977113f564d882668ce16
yisus8516 <32582536+yisus8516@users.noreply.github.com>
2017-10-22 21:01:45 -0500

    Add files via upload

---
+++ ex5.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define c 1
+#define f 0
+int l;
+float d;
+float res;
+int main() 
+{
+     printf("Este es un programa para calcular la equivalencia de grados centigrados a farenheigt y viceversa. \n \n Inroduzca la cantidad de grados seguido de su tipo 1-centigrados 2-farenheigt: \n");
+     scanf("%f %d",&d,&l);
+	 res=(l==1?(((1.8)*d)+32):(d-32)*(1.8));
+    	if (l==1)
+  	      {printf("\n El resultado es %f °F",res);
+	 
+         }
+        else
+         {printf("\n El resultado es %f °C",res);
+	
+         }
+	
+	system ("pause");
+	return 0;
+}
