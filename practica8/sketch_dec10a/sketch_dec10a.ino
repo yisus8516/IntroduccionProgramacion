6b4c6f9fef68226094f2cb8655882478645873dd
yisus8516 <32582536+yisus8516@users.noreply.github.com>
2017-12-10 22:15:48 -0600

    Add files via upload

---
+++ practica8/sketch_dec10a/sketch_dec10a.ino
@@ -0,0 +1,17 @@
+int sensorPin = 0; //pin analógico
+
+void setup() //inicializa
+{
+Serial.begin(9600); //Comienza conexión serial
+}
+void loop() //Ejecución continua
+{
+int reading = analogRead(sensorPin);
+//convertir a voltaje
+float voltage = reading * 5.0;
+voltage /= 1024.0;
+float temperatureC = (voltage - 0.5) * 100 ;
+Serial.print(temperatureC); //envía por serial
+Serial.println(" degrees C");
+delay(1000); //pausa de un seg
+}
